f7a3fd4b4d11baba7b69a38200c74eae5c8e28ce
Alex McHale <alex@anticlever.com>
2014-08-23 22:14:31 -0500

    get system init working again

--- lib/rubyku/remote_procedure.rb
+++ lib/rubyku/remote_procedure.rb
@@ -5,11 +5,14 @@ module Rubyku
     include FileUtils
     include Term::ANSIColor
 
-    attr_reader :hostname, :options
+    attr_reader :server_hostname, :options
 
-    def initialize(hostname, options = {})
-      @hostname = hostname
-      @options  = options
+    def initialize(server_hostname, options = {})
+      @server_hostname = server_hostname.to_s.strip
+      @options         = options
+
+      raise RubykuError, "server_hostname is not configured" if @server_hostname == ""
+      resolve_host! @server_hostname
     end
 
     def esc(string)
@@ -20,7 +23,7 @@ module Rubyku
       puts magenta(command.gsub(/^\s+/, "").gsub(/ +/, " ").strip)
       puts
 
-      Net::SSH.start(@hostname, username) do |ssh|
+      Net::SSH.start(server_hostname, username) do |ssh|
         stdout_data = ""
         stderr_data = ""
         exit_code   = nil
@@ -82,6 +85,12 @@ module Rubyku
       ssh_write_file(username, remote_path, read_template_file(template, template_options))
     end
 
+    def ssh_path_exists(username, remote_path)
+      ssh(app_username, "test -e #{ esc remote_path }") do |out, err, code, signal|
+        return code == 0
+      end
+    end
+
     def log(message)
       puts
       puts "#{ yellow '---->' } #{ green message.to_s }"
@@ -89,28 +98,66 @@ module Rubyku
     end
 
     def die(message)
-      log red(message.to_s)
-      exit 1
+      raise RubykuError, message.to_s
+    end
+
+    def all_templates
+      @@templates_path ||=
+        File.expand_path("../../../templates", __FILE__)
+
+      @@all_templates ||=
+        Dir["#{ @@templates_path }/**/*"].inject({}) do |templates, filename|
+          if File.file? filename
+            templates[File.basename filename] = File.read(filename)
+          end
+
+          templates
+        end
     end
 
     def read_template_file(filename, replacements = {})
-      path   = File.expand_path("../../../templates/#{ filename }", __FILE__)
-      string = File.read(path)
+      # Build the replacements that every procedure has.
+      all_replacements =
+        replacements.merge(all_templates).merge({
+          :app_username    => app_username              ,
+          :app_home        => "/home/#{ app_username }" ,
+          :server_hostname => server_hostname           ,
+        })
 
-      replacements.merge({
-        :app          => remote_project_name                                ,
-        :app_username => app_username                                       ,
-        :app_home     => "/home/#{ app_username }"                          ,
-        :app_root     => "/home/#{ app_username }/#{ remote_project_name }" ,
-        :hostname     => hostname                                           ,
-      }).inject(string) do |string, (key, value)|
-        string
-          .gsub("%%esc:#{ key }%%", esc(value.to_s))
-          .gsub("%%#{ key }%%", value.to_s)
+      # Append ones that exist for some procedures.
+      %i( app_name app_root app_hostname ).each do |key|
+        if respond_to? key
+          all_replacements[key] = send(key)
+        end
+      end
+
+      # A lambda to do the replacements as the "inject" keyword is recursive.
+      inject = -> string do
+        all_replacements.inject(string) do |str, (key, value)|
+          [
+            [ "%%inject:#{ key }%%" , -> { esc(inject[value]) } ],
+            [ "%%esc:#{ key }%%"    , -> { esc(value.to_s)    } ],
+            [ "%%#{ key }%%"        , -> { value.to_s         } ],
+          ].inject(str) do |str, (anchor, replacement)|
+            if str.index anchor
+              str.gsub(anchor, replacement[])
+            else
+              str
+            end
+
+          end
+        end
+      end
+
+      # Find the template and inject it.
+      if string = all_templates[filename]
+        inject[string]
+      else
+        raise RubykuError, "cannot find template #{ filename.inspect }"
       end
     end
 
-    def read_project_config(key)
+    def read_project_config key
       check_and_chdir_git! local_git_path
 
       yaml_filename = File.join(local_git_path, "config", "rubyku.yml")
@@ -119,38 +166,58 @@ module Rubyku
       yaml_hash[key]  if yaml_hash
     end
 
-    def yesno(prompt = 'Continue?', default = false)
-      a = ''
-      s = default ? '[Y/n]' : '[y/N]'
-      d = default ? 'y' : 'n'
-      until %w[y n].include? a
-        a = ask("#{prompt} #{s} ") { |q| q.limit = 1; q.case = :downcase }
-        a = d if a.length == 0
-      end
-      a == 'y'
-    end
-
-    def check_and_chdir_git!(local_git_path)
+    def check_and_chdir_local_app! local_git_path
       # Check that the path exists
       if !File.directory?(File.join(local_git_path, ".git"))
-        die "#{ local_git_path } is not a git repository"
+        raise RubykuError, "#{ local_git_path } is not a git repository"
       else
         chdir local_git_path
       end
 
       # Check that config/database.yml isn't checked in, is ignored
       if system("git ls-files config/database.yml --error-unmatch > /dev/null 2>&1")
-        die "database.yml is checked into git and should not be"
+        raise RubykuError, "database.yml is checked into git and should not be"
       end
 
       # Check that config/database.yml isn't checked in, is ignored
       if !system("git check-ignore config/database.yml > /dev/null 2>&1")
-        die "database.yml is not ignored by git and should be"
+        raise RubykuError, "database.yml is not ignored by git and should be"
+      end
+
+      # Check that Gemfile is committed
+      if !system("git ls-files Gemfile --error-unmatch > /dev/null 2>&1")
+        raise RubykuError, "repository must have a Gemfile committed"
+      end
+
+      # Check that config.ru is committed
+      if !system("git ls-files config.ru --error-unmatch > /dev/null 2>&1")
+        raise RubykuError, "repository must have a rack.ru file committed"
       end
 
       # Test that the proj has .ruby-version
       if !File.file?(".ruby-version")
-        die "repository must have a .ruby-version file"
+        raise RubykuError, "repository must have a .ruby-version file"
+      end
+    end
+
+    def resolve_host! hostname
+      Resolv.getaddress(hostname)
+    rescue Resolv::ResolvError
+      raise RubykuError, "invalid hostname #{ hostname.inspect }"
+    end
+
+    def verify_candidate_hostname! hostname
+      # Test the specified hostname against this regex.
+      regex = /(?=^.{4,255}$)(^((?!-)[a-zA-Z0-9-]{1,63}(?<!-)\.)+[a-zA-Z]{2,63}$)/
+      return true if hostname =~ regex
+
+      # Valid hostnames are always acceptable.
+      resolve_host! hostname
+    end
+
+    def check_symbol_name! name
+      if name !~ /\A[a-z0-9][a-z0-9_\-\.]*\z/
+        raise RubykuError, "#{ name.inspect } is not a valid name"
       end
     end
 
