5da6fa759e89ec2c5b73ebfeccdbb7687f343a61
Alex McHale <alex@anticlever.com>
2014-08-28 04:08:13 -0500

    fix how we reload nginx config

--- templates/nginx.configure-app.sh
+++ templates/nginx.configure-app.sh
@@ -9,7 +9,5 @@ echo %%inject:nginx.template.conf%% > /etc/nginx/sites-available/%%app_name%%
 perl -p -i -e "s/%%port%%/$port/g" /etc/nginx/sites-available/%%app_name%%
 ln -sf /etc/nginx/sites-available/%%app_name%% /etc/nginx/sites-enabled/%%app_name%%
 
-# Kill HUP nginx to reload its configuration
-if [ -f /run/nginx.pid ]; then
-  kill -HUP $( cat /run/nginx.pid )
-fi
+# Tell Nginx to load the new configuration
+service nginx reload
